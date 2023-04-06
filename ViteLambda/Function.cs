3bdd84d64d2f7a8066418889164f993078f35929
Byron Miller <byron.miller@slalom.com>
2023-04-06 17:17:38 -0400

    Add dynamo db first call attempt

--- ViteLambda/Function.cs
+++ ViteLambda/Function.cs
@@ -4,6 +4,8 @@ using System.Net;
 using System.Net.Http.Headers;
 using System.Net.Mime;
 using System.Reflection.Emit;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -19,9 +21,15 @@ public class Function
     /// <param name="input"></param>
     /// <param name="context"></param>
     /// <returns>APIGatewayProxyResponse</returns>
-    public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyResponse input, ILambdaContext context)
+    public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyResponse input, ILambdaContext context)
     {
-        Console.WriteLine("what is input: ", input);
+
+        var dbContext = new DynamoDBContext(new AmazonDynamoDBClient());
+
+        var submissionCollection = await dbContext.LoadAsync<SubmissionCollection>("submission_list");
+
+        Console.WriteLine("what is submissionCollection: ", submissionCollection);
+        
         return new APIGatewayProxyResponse
         {
             Body = input.Body,
@@ -33,4 +41,8 @@ public class Function
             },
         };
     }
+}
+
+public class SubmissionCollection {
+    public List<int>? Collection {get; set;}
 }
