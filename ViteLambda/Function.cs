4d65d175c518b54c6ea3b2e62590bd895803b4df
Byron Miller <byron.miller@slalom.com>
2023-04-07 18:17:13 -0400

    GET & POST examples | Not ViteLambda Related

--- ViteLambda/Function.cs
+++ ViteLambda/Function.cs
@@ -14,20 +14,74 @@ namespace ViteLambda;
 
 public class Function
 {
+  private readonly DynamoDBContext _dbContext;
 
+  public Function()
+  {
+    _dbContext = new DynamoDBContext(new AmazonDynamoDBClient());
+
+  }
   /// <summary>
   /// A simple function that takes an input and returns it as a response.
   /// </summary>
-  /// <param name="input"></param>
+  /// <param name="request"></param>
   /// <param name="context"></param>
   /// <returns>APIGatewayProxyResponse</returns>
-  public async Task<User> FunctionHandler(Guid input, ILambdaContext context)
+  public async Task<APIGatewayHttpApiV2ProxyResponse> FunctionHandler(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
   {
 
-    var dbContext = new DynamoDBContext(new AmazonDynamoDBClient());
-    var user = await dbContext.LoadAsync<User>(input);
+    return request.RequestContext.Http.Method.ToUpper() switch
+    {
+      "GET" => await HandleGetRequest(request),
+      "POST" => await HandlePostRequest(request)
+    };
+  }
 
-    return user;
+  private async Task<APIGatewayHttpApiV2ProxyResponse> HandlePostRequest(APIGatewayHttpApiV2ProxyRequest request)
+  {
+    var user = System.Text.Json.JsonSerializer.Deserialize<User>(request.Body);
+
+    if (user == null)
+    {
+      return BadResponse("Invalid User details");
+    }
+
+    await _dbContext.SaveAsync(user);
+    return new APIGatewayHttpApiV2ProxyResponse()
+    {
+      StatusCode = (int)HttpStatusCode.OK
+    };
+  }
+
+  private static APIGatewayHttpApiV2ProxyResponse BadResponse(string message)
+  {
+    return new APIGatewayHttpApiV2ProxyResponse()
+    {
+      Body = message,
+      StatusCode = (int)HttpStatusCode.NotFound
+    };
+  }
+
+  private async Task<APIGatewayHttpApiV2ProxyResponse> HandleGetRequest(APIGatewayHttpApiV2ProxyRequest request)
+  {
+    request.PathParameters.TryGetValue("userId", out var userIdString);
+    if (Guid.TryParse(userIdString, out var userId))
+    {
+
+      var dbContext = new DynamoDBContext(new AmazonDynamoDBClient());
+      var user = await _dbContext.LoadAsync<User>(userId);
+
+      if (user != null)
+      {
+        return new APIGatewayHttpApiV2ProxyResponse()
+        {
+          Body = System.Text.Json.JsonSerializer.Serialize(user),
+          StatusCode = (int)HttpStatusCode.OK
+        };
+      }
+    }
+
+    return BadResponse("Invalid userId in path");
   }
 }
 
@@ -35,7 +89,7 @@ public class Function
 
 public class User
 {
-    [DynamoDBHashKey]
+  [DynamoDBHashKey]
   public Guid Id { get; set; }
   public string? Name { get; set; }
 }
