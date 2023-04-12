7e7511d02ff41a31e202f74a5e98345a66e6757d
Byron Miller <byron.miller@slalom.com>
2023-04-12 17:39:22 -0400

    Finally returning from custom DB table. Fix the list next.

---
+++ submission-counter/Functions.cs
@@ -0,0 +1,138 @@
+using Amazon.Lambda.Core;
+using Amazon.Lambda.Annotations;
+using Amazon.Lambda.Annotations.APIGateway;
+using System.Text.Json.Serialization;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2;
+using System.Net;
+using Amazon.Lambda.APIGatewayEvents;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace submission_counter;
+
+/// <summary>
+/// A collection of sample Lambda functions that provide a REST api for doing simple math calculations. 
+/// </summary>
+public class Functions
+{
+    private readonly DynamoDBContext _dbContext;
+    private readonly Guid _connectString;
+
+    /// <summary>
+    /// Default constructor.
+    /// </summary>
+    public Functions()
+    {
+        _dbContext = new DynamoDBContext(new AmazonDynamoDBClient());
+        _connectString = Guid.Parse("0405ee69-2efd-43ec-925a-086e25bf6459");
+    }
+
+
+    [LambdaFunction]
+    [HttpApi(LambdaHttpMethod.Get, "/submissions")]
+    public async Task<APIGatewayHttpApiV2ProxyResponse> GetListAsync() {
+        Console.WriteLine("_connectString $$$: ", _connectString);
+        var list = await _dbContext.LoadAsync<SubmissionCollection>(_connectString);
+        //var list = 0;
+        if (list != null)
+        {
+            return new APIGatewayHttpApiV2ProxyResponse()
+            {
+                StatusCode = (int)HttpStatusCode.OK,
+                Body = System.Text.Json.JsonSerializer.Serialize(list)
+            };
+        }
+
+        return new APIGatewayHttpApiV2ProxyResponse()
+        {
+            StatusCode = (int)HttpStatusCode.BadGateway,
+            Body = "Bad Gateway - bah!"
+
+        };
+        
+    }
+
+    /// <summary>
+    /// Root route that provides information about the other requests that can be made.
+    /// </summary>
+    /// <returns>API descriptions.</returns>
+    [LambdaFunction()]
+    [HttpApi(LambdaHttpMethod.Get, "/")]
+    public string Default()
+    {
+        var docs = @"Lambda Calculator Home:
+You can make the following requests to invoke other Lambda functions perform calculator operations:
+/add/{x}/{y}
+/subtract/{x}/{y}
+/multiply/{x}/{y}
+/divide/{x}/{y}
+";
+        return docs;
+    }
+
+    /// <summary>
+    /// Perform x + y
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <returns>Sum of x and y.</returns>
+    [LambdaFunction()]
+    [HttpApi(LambdaHttpMethod.Get, "/add/{x}/{y}")]
+    public int Add(int x, int y, ILambdaContext context)
+    {
+        context.Logger.LogInformation($"{x} plus {y} is {x + y}");
+        return x + y;
+    }
+
+    /// <summary>
+    /// Perform x - y.
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <returns>x subtract y</returns>
+    [LambdaFunction()]
+    [HttpApi(LambdaHttpMethod.Get, "/subtract/{x}/{y}")]
+    public int Subtract(int x, int y, ILambdaContext context)
+    {
+        context.Logger.LogInformation($"{x} subtract {y} is {x - y}");
+        return x - y;
+    }
+
+    /// <summary>
+    /// Perform x * y.
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <returns>x multiply y</returns>
+    [LambdaFunction()]
+    [HttpApi(LambdaHttpMethod.Get, "/multiply/{x}/{y}")]
+    public int Multiply(int x, int y, ILambdaContext context)
+    {
+        context.Logger.LogInformation($"{x} multiply {y} is {x * y}");
+        return x * y;
+    }
+
+    /// <summary>
+    /// Perform x / y.
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <returns>x divide y</returns>
+    [LambdaFunction()]
+    [HttpApi(LambdaHttpMethod.Get, "/divide/{x}/{y}")]
+    public int Divide(int x, int y, ILambdaContext context)
+    {
+        context.Logger.LogInformation($"{x} divide {y} is {x / y}");
+        return x / y;
+    }
+}
+
+    public class SubmissionCollection
+{
+    [DynamoDBHashKey]
+    public Guid Id;
+
+        [JsonPropertyName("list")]
+        public List<int>? List { get; set; }
+    }
