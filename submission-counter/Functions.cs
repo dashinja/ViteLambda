7aa84917c999dd31a461ba563249f67d69fb606c
Byron Miller <byron.miller@slalom.com>
2023-04-14 17:44:12 -0400

    AddPostAnnotationForSubmission (#2)
    * Add post
    
    * Maintain single record
    
    * Added needed things.
    
    * meh - fix post and be done with BE for now
    
    * Pick it up tomorrow: Test POST again.
    
    * GET, POST, and DELETE work again.
    However - Order of POST list gets off after two submissions.
    
    * Still needs list retrieval order fixed from GET calls. (maybe issue is sourcedi n POST calls)

--- submission-counter/Functions.cs
+++ submission-counter/Functions.cs
@@ -6,6 +6,7 @@ using Amazon.DynamoDBv2.DataModel;
 using Amazon.DynamoDBv2;
 using System.Net;
 using Amazon.Lambda.APIGatewayEvents;
+using System.Security.Cryptography.X509Certificates;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
@@ -16,139 +17,171 @@ namespace submission_counter;
 /// </summary>
 public class Functions
 {
-    private readonly DynamoDBContext _dbContext;
-    private readonly Guid _connectString;
-    /// <summary>
-    /// Default constructor.
-    /// </summary>
-    public Functions()
+  private readonly DynamoDBContext _dbContext;
+  private readonly Guid _connectString;
+  /// <summary>
+  /// Default constructor.
+  /// </summary>
+  public Functions()
+  {
+    _dbContext = new DynamoDBContext(new AmazonDynamoDBClient());
+
+    //TODO: Learn to put this in environment variable.
+    _connectString = Guid.Parse("0405ee69-2efd-43ec-925a-086e25bf6459");
+  }
+
+  /// <summary>
+  /// Endpoint that returns the submission list to the caller.
+  /// </summary>
+  /// <returns>APIGatewayHttpApiV2ProxyResponse</returns>
+  [LambdaFunction]
+  [HttpApi(LambdaHttpMethod.Get, "/submissions")]
+  public async Task<APIGatewayHttpApiV2ProxyResponse> GetListAsync()
+  {
+    SubmissionCollection list = await GetListValue();
+
+    if (list != null)
     {
-        _dbContext = new DynamoDBContext(new AmazonDynamoDBClient());
-        
-        //TODO: Learn to put this in environment variable.
-        _connectString = Guid.Parse("0405ee69-2efd-43ec-925a-086e25bf6459");
+      return GoodResponse(list.List);
     }
 
+    return BadRequest("List not found", HttpStatusCode.NotFound);
 
-    [LambdaFunction]
-    [HttpApi(LambdaHttpMethod.Get, "/submissions")]
-    public async Task<APIGatewayHttpApiV2ProxyResponse> GetListAsync()
+  }
+
+  /// <summary>
+  /// Adds new data to the submission list.
+  /// </summary>
+  [LambdaFunction]
+  [HttpApi(LambdaHttpMethod.Post, "/submissions")]
+  public async Task<APIGatewayHttpApiV2ProxyResponse> PostListAsync([FromBody] RequestBody request, ILambdaContext context)
+  {
+
+    Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(request));
+    Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(request.Body));
+
+    try
     {
-        SubmissionCollection list = await GetListValue();
+      var user = await GetListValue();
 
-        if (list != null)
+      if (user != null)
+      {
+        user.List.Add(request.Body);
+
+        try
         {
-            return GoodResponse(list);
+          PostListValue(user).Wait();
+
+          return GoodResponse(user.List);
+        }
+        catch (Exception e)
+        {
+          return BadRequest($"Failed to get Value from PostListValue - Message: {e.Message}", HttpStatusCode.InternalServerError);
         }
 
-        return BadRequest();
-
+      }
     }
-
-    private static APIGatewayHttpApiV2ProxyResponse GoodResponse(SubmissionCollection list)
+    catch (Exception e)
     {
+      Console.WriteLine($"Error Message: ");
+      return BadRequest($"Submission Failed to Complete - Error: {e.Message}", HttpStatusCode.BadRequest);
+    }
+    return BadRequest("Failed to save new submission");
+  }
+
+  [LambdaFunction]
+  [HttpApi(LambdaHttpMethod.Delete, "/submissions")]
+
+  public async Task<APIGatewayHttpApiV2ProxyResponse> DeleteListAsync(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
+  {
+    Console.WriteLine("What is the method?$$$$$");
+    Console.WriteLine(request.RequestContext.Http.Method);
+
+    if (request.RequestContext.Http.Method == "DELETE")
+    {
+      try
+      {
+        await _dbContext.DeleteAsync<SubmissionCollection>(_connectString);
+
         return new APIGatewayHttpApiV2ProxyResponse()
         {
-            StatusCode = (int)HttpStatusCode.OK,
-            Body = System.Text.Json.JsonSerializer.Serialize(list)
+          Body = "List deleted",
+          StatusCode = (int)HttpStatusCode.NoContent
         };
+      }
+      catch (Exception)
+      {
+        throw new Exception("Problem deleting Item");
+      }
     }
 
-    private static APIGatewayHttpApiV2ProxyResponse BadRequest()
+    return BadRequest("Incorrect Delete Request", HttpStatusCode.BadRequest);
+
+  }
+
+  private static APIGatewayHttpApiV2ProxyResponse GoodResponse(List<int> list)
+  {
+    return new APIGatewayHttpApiV2ProxyResponse()
     {
-        return new APIGatewayHttpApiV2ProxyResponse()
-        {
-            StatusCode = (int)HttpStatusCode.BadGateway,
-            Body = "Bad Gateway - bah!"
+      StatusCode = (int)HttpStatusCode.OK,
+      Body = System.Text.Json.JsonSerializer.Serialize(list)
+    };
+  }
 
-        };
-    }
-
-    private async Task<SubmissionCollection> GetListValue()
+  private static APIGatewayHttpApiV2ProxyResponse BadRequest(String message, HttpStatusCode statusCode = HttpStatusCode.InternalServerError)
+  {
+    return new APIGatewayHttpApiV2ProxyResponse()
     {
-        return await _dbContext.LoadAsync<SubmissionCollection>(_connectString);
-    }
+      StatusCode = (int)statusCode,
+      Body = message
+    };
+  }
 
-    /// <summary>
-    /// Root route that provides information about the other requests that can be made.
-    /// </summary>
-    /// <returns>API descriptions.</returns>
-    [LambdaFunction()]
-    [HttpApi(LambdaHttpMethod.Get, "/")]
-    public string Default()
-    {
-        var docs = @"Lambda Calculator Home:
-You can make the following requests to invoke other Lambda functions perform calculator operations:
-/add/{x}/{y}
-/subtract/{x}/{y}
-/multiply/{x}/{y}
-/divide/{x}/{y}
-";
-        return docs;
-    }
+  /// <summary>
+  /// Retrieves current value list store and returns it to the caller.
+  /// </summary>
+  /// <returns>SubmissionCollection</returns>
+  private async Task<SubmissionCollection> GetListValue()
+  {
+    var result = await _dbContext.LoadAsync<SubmissionCollection>(_connectString);
 
-    /// <summary>
-    /// Perform x + y
-    /// </summary>
-    /// <param name="x"></param>
-    /// <param name="y"></param>
-    /// <returns>Sum of x and y.</returns>
-    [LambdaFunction()]
-    [HttpApi(LambdaHttpMethod.Get, "/add/{x}/{y}")]
-    public int Add(int x, int y, ILambdaContext context)
+    if (result != null)
     {
-        context.Logger.LogInformation($"{x} plus {y} is {x + y}");
-        return x + y;
+      return result;
     }
+    else
+    {
+      var init_submission = new SubmissionCollection() { Id = _connectString, List = { } };
 
-    /// <summary>
-    /// Perform x - y.
-    /// </summary>
-    /// <param name="x"></param>
-    /// <param name="y"></param>
-    /// <returns>x subtract y</returns>
-    [LambdaFunction()]
-    [HttpApi(LambdaHttpMethod.Get, "/subtract/{x}/{y}")]
-    public int Subtract(int x, int y, ILambdaContext context)
-    {
-        context.Logger.LogInformation($"{x} subtract {y} is {x - y}");
-        return x - y;
-    }
+      await _dbContext.SaveAsync(init_submission);
 
-    /// <summary>
-    /// Perform x * y.
-    /// </summary>
-    /// <param name="x"></param>
-    /// <param name="y"></param>
-    /// <returns>x multiply y</returns>
-    [LambdaFunction()]
-    [HttpApi(LambdaHttpMethod.Get, "/multiply/{x}/{y}")]
-    public int Multiply(int x, int y, ILambdaContext context)
-    {
-        context.Logger.LogInformation($"{x} multiply {y} is {x * y}");
-        return x * y;
+      return await _dbContext.LoadAsync<SubmissionCollection>(_connectString);
     }
+  }
 
-    /// <summary>
-    /// Perform x / y.
-    /// </summary>
-    /// <param name="x"></param>
-    /// <param name="y"></param>
-    /// <returns>x divide y</returns>
-    [LambdaFunction()]
-    [HttpApi(LambdaHttpMethod.Get, "/divide/{x}/{y}")]
-    public int Divide(int x, int y, ILambdaContext context)
-    {
-        context.Logger.LogInformation($"{x} divide {y} is {x / y}");
-        return x / y;
-    }
+  private async Task PostListValue(SubmissionCollection newSubmission)
+  {
+    await _dbContext.SaveAsync(newSubmission);
+  }
 }
 
-    public class SubmissionCollection
+public class SubmissionCollection
 {
-    [DynamoDBHashKey]
-    public Guid Id;
+  public SubmissionCollection()
+  {
+    Id = Guid.NewGuid();
+    List = new List<int> { };
+  }
 
-        [JsonPropertyName("list")]
-        public List<int>? List { get; set; }
-    }
+  [DynamoDBHashKey]
+  public Guid Id;
+
+  [JsonPropertyName("list")]
+  public List<int> List { get; set; }
+}
+
+public class RequestBody
+{
+  [JsonPropertyName("body")]
+  public int Body { get; set; }
+}
